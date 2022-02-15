0260f56f47ceb1f8c662e1b7f7eacd9b66db09b8
BT_1E10_03 <sample@bths.edu>
2022-02-15 10:59:20 -0500

    Finished search

--- src/Search.java
+++ src/Search.java
@@ -6,16 +6,38 @@ public class Search {
     // a target int is found in an array of ints (both the target and int array
     // should be accepted as parameters); return -1 if target is not found
     // BE SURE TO USE EARLY RETURN (more efficient)
-
-
-
+    public static int linearSearchInt(int[] elements, int target)
+    {
+        int num = -1;
+        for(int i = 0; i<elements.length; i++)
+        {
+            if(elements[i] == target)
+            {
+                num = i;
+                return num;
+            }
+        }
+        return num;
+    }
 
 
     // B. write a second static method that has the same parameters as linearSearch but
     // instead of returning an index, it returns true if target is found or false
     // if target is not found
     // AGAIN, BE SURE TO USE EARLY RETURN
-
+    public static boolean linearSearchBoolean(int[] elements, int target)
+    {
+        boolean contain = false;
+        for(int i = 0; i<elements.length; i++)
+        {
+            if(elements[i] == target)
+            {
+                contain = true;
+                return contain;
+            }
+        }
+        return contain;
+    }
 
 
 
@@ -24,24 +46,51 @@ public class Search {
     // of Integers (rather than an array of ints) and a target int; return the first index
     // at which the target is found, or return -1 if target is not found
     // BE SURE TO USE EARLY RETURN
-
-
-
+    public static int linearSearchInteger(ArrayList<Integer> elements, int target)
+    {
+        int num = -1;
+        for(int i = 0; i<elements.size(); i++)
+        {
+            if(elements.get(i) == target)
+            {
+                num = i;
+                return num;
+            }
+        }
+        return num;
+    }
 
 
     // D. provide another OVERLOADED version of the linearSearch method that accepts an array
     // of Strings and a target String; return the first index at which the target is found,
     // or return -1 if target is not found
     // BE SURE TO USE EARLY RETURN
-
-
-
+    public static int linearSearchString(String[] elements, String target)
+    {
+        for(int i = 0; i<elements.length; i++){
+            if(elements[i].equals(target))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 
     // E. write a "linearSearchLast" method that accepts an array of ints and a
     // target int, and return the LAST index at which the target is found,
     // or return -1 if target is not found
-
+    public static int linearSearchLast(int[] elements, int target)
+    {
+        for(int i = elements.length-1; i > 0 ; i--)
+        {
+            if(elements[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 
 
@@ -50,8 +99,28 @@ public class Search {
     // accepts an array of ints and a target int, and the other that accepts an ArrayList
     // of Integers and a target int.  Each method should return the NUMBER OF TIMES
     // the target appears in the array/ArrayList.
+    public static int linearSearchCount(int[] elements, int target)
+    {
+        int count = 0;
+        for(int i = 0; i<elements.length; i++){
+            if(elements[i]==(target))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 
 
-
-
+    public static int linearSearchCount(ArrayList<Integer> elements, int target)
+    {
+        int count = 0;
+        for(int i = 0; i<elements.size(); i++){
+            if(elements.get(i)==(target))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 }
